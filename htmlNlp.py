839a0b3ec6fc1296cfbd21c5e00074a674762ad0
ungentilgarçon <gregory.bahde@laposte.net>
2015-11-26 14:10:46 +0100

    starting to Nlp the web

--- htmlNlp.py
+++ htmlNlp.py
@@ -1,7 +1,9 @@
+#python3 code
 import nltk, re, pprint
 from nltk import word_tokenize
 from urllib import request
 from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 url = "http://aadn.org/"
 # //response = request.urlopen(url)
 # //raw=  response.read().decode('utf8')
@@ -10,4 +12,9 @@ req = Request( url, headers={'User-Agent': 'Mozilla/5.0'})
 html = urlopen(req).read().decode('utf8')
 # print(response)
 # print(raw)
-print (html)
+# print (html)
+raw = BeautifulSoup(html).get_text()
+tokens = word_tokenize(raw)
+# print(tokens)
+text = nltk.Text(tokens)
+text.concordance('aadn')
