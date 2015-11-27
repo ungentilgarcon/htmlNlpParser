1f701aadc9a945f2e48a0919fac2466ba238f93f
ungentilgarcon <monsieur.goonie@gmail.com>
2015-11-27 02:29:06 +0100

    not working but too late, threading

--- htmlNlp.py
+++ htmlNlp.py
@@ -1,3 +1,4 @@
+#GPL V3
 import sys
 import urllib
 from urllib import parse
@@ -10,6 +11,7 @@ from nltk import word_tokenize
 from urllib import request
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from threading import Thread
 import requests
 import re
 
@@ -30,34 +32,64 @@ def processlinks(url):
     page.close()
 
     soup = BeautifulSoup(text)
-
+    hrefReturn = []
     for tag in soup.findAll('a', href=True):
         tag['href'] = urllib.parse.urljoin(url, tag['href'])
-        print (tag['href'])
+        hrefReturn.append(tag['href'])
+        # print(hrefReturn)
+    return hrefReturn
 # process(url)
 
 
 
+#TRYING TO GO THE THREAD WAY 1/2
+#def parse_text(url, table): 
+class parse_text(Thread):
 
-def parse_text(url, table): 
-	print(url)
-	# //response = request.urlopen(url)
-	# //raw=  response.read().decode('utf8')
+
+
+    def __init__(self,url, table): 
+
+        Thread.__init__(self)
+
+        self.url = url
+        self.table = table
+		print(url)
+		# //response = request.urlopen(url)
+		# //raw=  response.read().decode('utf8')
+		# html = request.urlopen(url).read().decode('utf8')
+		#GET TREE
+		urllinked = processlinks(url)
+		#AND LETS LOOP... 
+		for link in urllinked:
+			parsed_uri1 = urlparse( link )
+			parsed_uri2 = urlparse( url )
+			domain1 ='{uri.netloc}'.format(uri=parsed_uri1)
+			domain2 ='{uri.netloc}'.format(uri=parsed_uri2)
+				#...BUT PROCESS ONLY IF URLS MATCH
+			if domain1 == domain2:
+				urllinked2 = processlinks(link)
+		#CHECK IF A THOUSAND OR MORE? IF NOT RERUN,IF SO CUT AT 999
+
+	urllinked.append(url)
 	req = Request( url, headers={'User-Agent': 'Mozilla/5.0'})
-	# html = request.urlopen(url).read().decode('utf8')
 	html = urlopen(req).read()#.decode('utf8')
-	#FOR TESTING IF NEEDED
-	# print(response)
-	# print(raw)
-	# print (html)
-	#GET TREE
-	processlinks(url)
 	raw = BeautifulSoup(html, "html5lib").get_text()
+		#FOR TESTING IF NEEDED
+		# print(response)
+		# print(raw)
+		# print (html)
+
+
+
+
+#THE NLTK PART,BETTER FITTED SOMEWHERE ELSE
 	tokens = word_tokenize(raw)
 	# print(tokens)
 	text = nltk.Text(tokens)
 	with open('./data/liste_sigles.csv', newline='') as csvfile:
 		siglereader=csv.reader(csvfile, delimiter=',', quotechar='|')
+		#SKIP HEADER
 		next(siglereader, None) 
 		for row in siglereader:
 			print (url,">>>>>>>>>>",row[0])
@@ -73,11 +105,23 @@ def parse_text(url, table):
 
 with open('./data/liste_sites.csv', newline='') as csvfile:
 	urlreader=csv.reader(csvfile, delimiter=',', quotechar='|')
+	#SKIP HEADER
 	next(urlreader, None) 
 
-	for row in urlreader:
-		parse_text(row[0],urlreader)
+	#for row in urlreader:
+	#	parse_text(row[0],urlreader)
+#TRYING TO GO THE THREAD WAY 2/2
+	# Création des threads
+	thread_1 = Afficheur("1")
+	thread_2 = Afficheur("2")
 
+	# Lancement des threads
+	thread_1.start()
+	thread_2.start()
+
+	# Attend que les threads se terminent
+	thread_1.join()
+	thread_2.join()
 
 
 
