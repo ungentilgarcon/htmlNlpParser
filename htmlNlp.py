5aedbeb502696c7ed32cf29dd0629f8aa4d1bb11
ungentilgarcon <monsieur.goonie@gmail.com>
2015-11-27 13:22:18 +0100

    not working but almost

--- htmlNlp.py
+++ htmlNlp.py
@@ -1,7 +1,7 @@
 #GPL V3
 import sys
 import urllib
-from urllib import parse
+from urllib.parse import urlparse
 
 
 #python3 code
@@ -42,58 +42,76 @@ def processlinks(url):
 
 
 
-#TRYING TO GO THE THREAD WAY 1/2
-#def parse_text(url, table): 
-class parse_text(Thread):
+#TRYING TO GO THE THREAD WAY 1/2 #NOT NOW ;)
+def parse_text(url, table): 
+# class parse_text(Thread):
 
 
 
-    def __init__(self,url, table): 
+    # def __init__(self,url, table): 
 
-        Thread.__init__(self)
+    #     Thread.__init__(self)
 
-        self.url = url
-        self.table = table
+        # self.url = url
+        # self.table = table #WHAT WAS IT FOR ALREADY? ;)
 		print(url)
 		# //response = request.urlopen(url)
 		# //raw=  response.read().decode('utf8')
 		# html = request.urlopen(url).read().decode('utf8')
 		#GET TREE
-		urllinked = processlinks(url)
+		urlLinked = processlinks(url)
 		#AND LETS LOOP... 
-		for link in urllinked:
-			parsed_uri1 = urlparse( link )
-			parsed_uri2 = urlparse( url )
-			domain1 ='{uri.netloc}'.format(uri=parsed_uri1)
-			domain2 ='{uri.netloc}'.format(uri=parsed_uri2)
-				#...BUT PROCESS ONLY IF URLS MATCH
-			if domain1 == domain2:
-				urllinked2 = processlinks(link)
-		#CHECK IF A THOUSAND OR MORE? IF NOT RERUN,IF SO CUT AT 999....AND APPEND
+		while urlLinked.__len__() < 999:
+			
+			for link in urlLinked:
+				print(link)
+				print(url)
+				parsed_uri1 = urllib.parse.urlparse( link )
+				parsed_uri2 = urllib.parse.urlparse( url )
+				domain1 ='{uri.netloc}'.format(uri=parsed_uri1)
+				domain2 ='{uri.netloc}'.format(uri=parsed_uri2)
+					#...BUT PROCESS ONLY IF URLS MATCH
+				urlLinked2 = []
+				if domain1 == domain2:
+					try:
+						urlLinked2 = processlinks(link)
+						break
+					except ValueError:
+						print ("Oops!  That was no valid number.  Try again...")
+			#SELECT ONLY THE NEW ONES,CHECK IF A THOUSAND OR MORE? IF NOT RERUN AND APPEND,IF SO THEN CUT AT 999....AND APPEND
+				urlIdent = set(urlLinked).intersection(set(urlLinked2))
+				if urlIdent.__len__() > 0:
+					urlLinked2.remove(urlIdent)
+				if (urlLinked.__len__() + urlLinked2.__len__()) >= 999:
+					break
+				elif (urlLinked.__len__() + urlLinked2.__len__())< 999:
+					urlLinked.append(urlLinked2)
+					continue
 
-	urllinked.append(url)
-	req = Request( url, headers={'User-Agent': 'Mozilla/5.0'})
-	html = urlopen(req).read()#.decode('utf8')
-	raw = BeautifulSoup(html, "html5lib").get_text()
-		#FOR TESTING IF NEEDED
-		# print(response)
-		# print(raw)
-		# print (html)
 
-
-
-
+		while urlLinked2.__len__() > (999 -urlLinked):
+			urlLinked2.pop()
+		urlLinked.append(urlLinked2)
+		urlLinked.append(url)
+		for urls in urlLinked:
+			req = Request( urls, headers={'User-Agent': 'Mozilla/5.0'})
+			html = urlopen(req).read()#.decode('utf8')
+			raw = BeautifulSoup(html, "html5lib").get_text()
+				#FOR TESTING IF NEEDED
+				# print(response)
+				# print(raw)
+				# print (html)
 #THE NLTK PART,BETTER FITTED SOMEWHERE ELSE
-	tokens = word_tokenize(raw)
-	# print(tokens)
-	text = nltk.Text(tokens)
-	with open('./data/liste_sigles.csv', newline='') as csvfile:
-		siglereader=csv.reader(csvfile, delimiter=',', quotechar='|')
-		#SKIP HEADER
-		next(siglereader, None) 
-		for row in siglereader:
-			print (url,">>>>>>>>>>",row[0])
-			text.concordance(row[0])
+			tokens = word_tokenize(raw)
+			# print(tokens)
+			text = nltk.Text(tokens)
+			with open('./data/liste_sigles.csv', newline='') as csvfile:
+				siglereader=csv.reader(csvfile, delimiter=',', quotechar='|')
+				#SKIP HEADER
+				next(siglereader, None) 
+				for row in siglereader:
+					print (url,">>>>>>>>>>",row[0])
+					text.concordance(row[0])
 
 
 
@@ -108,8 +126,8 @@ with open('./data/liste_sites.csv', newline='') as csvfile:
 	#SKIP HEADER
 	next(urlreader, None) 
 
-	#for row in urlreader:
-	#	parse_text(row[0],urlreader)
+	for row in urlreader:
+		parse_text(row[0],urlreader)
 #TRYING TO GO THE THREAD WAY 2/2
 	# Création des threads
 	thread_1 = Afficheur("1")
